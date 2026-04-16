ad66104bcd2b67ab6a56873b7d0d8b174c2c4b9d
BogdanNechifor <93227187+BogdanNechifor@users.noreply.github.com>
2026-04-16 15:46:07 +0300

    Update AiArea.cs

--- Assets/Code/Scripts/Player/AI/AiArea.cs
+++ Assets/Code/Scripts/Player/AI/AiArea.cs
@@ -1,13 +1,18 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Player.AI.Navigation
 {
+    
     [RequireComponent(typeof(BoxCollider2D))]
     public class AiArea : MonoBehaviour
     {
         [Tooltip("The patrol points within this area where the AI can navigate.")]
         public Transform[] anchorPoints;
 
+        [SerializeField] List<AiPoint> points;
+        
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (other.CompareTag("Player"))
@@ -15,7 +20,18 @@ namespace Player.AI.Navigation
                 AiManager.Instance.SetActiveArea(this);
             }
         }
+
+        private void OnValidate()
+        {
+            Debug.Log("OnValidate");
+        }
         
+        
+        [ContextMenu("Do Something")]
+        public void Test()
+        {
+            Debug.Log("Test",this);
+        }
         private void OnDrawGizmos()
         {
             if (anchorPoints == null) return;
@@ -30,4 +46,13 @@ namespace Player.AI.Navigation
             }
         }
     }
+}
+
+[System.Serializable]
+public class AiPoint
+{
+    public UnityEvent onArrive;
+    public int speed;
+    public Transform pos;
+    public float num2;
 }
